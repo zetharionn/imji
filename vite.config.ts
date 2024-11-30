29e307638cc962dc73ebd97e9cfaca677bdbef6d
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-11-30 18:43:53 +0300

    style: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+	plugins: [react()],
+	resolve: {
 		alias: {
 			'@app': path.resolve(__dirname, './src/app'),
 			'@pages': path.resolve(__dirname, './src/pages'),
