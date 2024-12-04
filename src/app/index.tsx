6584a47d84a52e0a80905d87f0c19d22efaa8be9
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-04 20:16:32 +0300

    feat: Analytics & SpeedInsights

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,15 @@
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import './ui'
 
 const App = () => {
-	return <div>imji</div>
+	return (
+		<>
+			<Analytics />
+			<div>Imji</div>
+			<SpeedInsights />
+		</>
+	)
 }
 
 export default App
