27072e01fa65db6975f23e3479c95612d59bea27
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-09 02:11:37 +0300

    chore: NextUI provider

---
+++ src/app/lib/providers/NextUI.tsx
@@ -0,0 +1,10 @@
+import { NextUIProvider } from '@nextui-org/react'
+import type { ComponentType } from 'react'
+
+export const NextUI = (Component: ComponentType) => {
+	return (
+		<NextUIProvider>
+			<Component />
+		</NextUIProvider>
+	)
+}
