88e4601f65d3beb8be6a3c1410d04f4c015ca1df
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-09 02:25:49 +0300

    chore: nextui routing

--- src/app/lib/providers/NextUI.tsx
+++ src/app/lib/providers/NextUI.tsx
@@ -1,9 +1,15 @@
 import { NextUIProvider } from '@nextui-org/react'
+import { useRouter } from '@tanstack/react-router'
 import type { ComponentType } from 'react'
 
 export const NextUI = (Component: ComponentType) => () => {
+	const router = useRouter()
+
 	return (
-		<NextUIProvider>
+		<NextUIProvider
+			navigate={to => router.navigate({ to })}
+			useHref={to => router.buildLocation({ to }).href}
+		>
 			<Component />
 		</NextUIProvider>
 	)
