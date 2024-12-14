81a6c7225c19284232acdd33977c8013eee9c0f7
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-14 23:16:11 +0300

    feat: upload

--- src/shared/services/Image/Image.service.ts
+++ src/shared/services/Image/Image.service.ts
@@ -1,10 +1,17 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
-import type { IImageService } from './Image.types'
+import type { FileBody, FileOptions, IImageService } from './Image.types'
 
 export class ImageService implements IImageService {
 	readonly supabaseClient: SupabaseClient
 
 	constructor(supabaseClient: SupabaseClient) {
 		this.supabaseClient = supabaseClient
+	}
+
+	async upload(path: string, fileBody: FileBody, fileOptions?: FileOptions) {
+		const { data, error } = await this.supabaseClient.storage
+			.from('images')
+			.upload(path, fileBody, fileOptions)
+		return { data, error }
 	}
 }
--- src/shared/services/Image/Image.types.ts
+++ src/shared/services/Image/Image.types.ts
@@ -2,4 +2,34 @@ import type { SupabaseClient } from '@supabase/supabase-js'
 
 export interface IImageService {
 	readonly supabaseClient: SupabaseClient
+
+	upload: (
+		path: string,
+		fileBody: FileBody,
+		fileOptions?: FileOptions
+	) => StorageResponse
 }
+
+export type FileBody =
+	| ArrayBuffer
+	| ArrayBufferView
+	| Blob
+	| Buffer
+	| File
+	| FormData
+	| NodeJS.ReadableStream
+	| ReadableStream<Uint8Array>
+	| URLSearchParams
+	| string
+
+export interface FileOptions {
+	cacheControl?: string
+	contentType?: string
+	duplex?: string
+	upsert?: boolean
+}
+
+export type StorageResponse = Promise<{
+	data: { id: string; path: string; fullPath: string } | null
+	error: null | Error
+}>
