13b12b625c7dc3a539ce7b0f55288936056b2758
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-16 22:28:18 +0300

    refactor: Uploader restyle

--- src/widgets/Uploader/ui/Uploader.tsx
+++ src/widgets/Uploader/ui/Uploader.tsx
@@ -2,7 +2,10 @@ import { FileButton } from '@features/FileButton'
 
 export const Uploader = () => {
 	return (
-		<div>
+		<div className='flex flex-col items-center justify-center gap-4'>
+			<div>
+				<h1 className='text-3xl font-bold'>Save and share your images</h1>
+			</div>
 			<FileButton />
 		</div>
 	)
