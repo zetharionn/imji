92e4f0a4b40b87b9556c319d81c4929f5d8361bf
zetharionn <zetharionn@gmail.com>
2024-12-16 22:26:14 +0300

    refactor: NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,9 @@
 export const NotFound = () => {
-	return <div>Not Found</div>
+	return (
+		<>
+			<div className='h-[80vh] flex flex-col items-center justify-center'>
+				<h1 className='text-3xl font-bold'>Not Found</h1>
+			</div>
+		</>
+	)
 }
