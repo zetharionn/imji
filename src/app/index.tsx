46e0b38bc3e05b5fdee85fa07b2c6f75fd786564
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-19 06:21:01 +0300

    refactor: styles as style app segment

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Routing } from './ui'
-import './ui'
+import './styles'
 
 export const App = () => {
 	return (
