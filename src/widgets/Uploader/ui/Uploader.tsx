2265dfd557d5adeabe1e30041e86b852f9f27aea
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-19 17:13:54 +0300

    fix: text style

--- src/widgets/Uploader/ui/Uploader.tsx
+++ src/widgets/Uploader/ui/Uploader.tsx
@@ -2,9 +2,11 @@ import { FileButton } from '@features/FileButton'
 
 export const Uploader = () => {
 	return (
-		<div className='flex flex-col items-center justify-center gap-4'>
+		<div className='flex flex-col text-center items-center justify-center gap-4'>
 			<div>
-				<h1 className='text-3xl font-bold'>Save and share your images</h1>
+				<h1 className='sm:text-2xl md:text-3xl lg:text-4xl font-bold'>
+					Save and share your images
+				</h1>
 			</div>
 			<FileButton />
 		</div>
