ce852d57ddf19a168dc709784933024eae748e02
zetharionn <zetharionn@gmail.com>
2024-12-19 18:25:19 +0300

    refactor: FileButton loading and mutation

--- src/features/FileButton/lib/hooks/useFileButton.ts
+++ src/features/FileButton/lib/hooks/useFileButton.ts
@@ -1,15 +1,22 @@
-import { storageService } from '@shared/services'
+import { useImagesStore } from '@entities/images'
+import { useMutation } from '@tanstack/react-query'
 import { type ChangeEvent, useRef } from 'react'
 
 export const useFileButton = () => {
+	const upload = useImagesStore(state => state.upload)
+
+	const mutation = useMutation({
+		mutationFn: upload
+	})
+
 	const ref = useRef<HTMLInputElement | null>(null)
 
 	const click = () => ref.current?.click()
 
 	const change = (event: ChangeEvent<HTMLInputElement>) => {
 		const file = event.currentTarget.files?.[0] as File
-		storageService.upload('images', file.name, file)
+		mutation.mutate(file)
 	}
 
-	return { ref, click, change }
+	return { ref, click, change, mutation }
 }
