d5253a0f757af48a3c542f13de6d1ad38f34cd7e
zetharionn <zetharionn@gmail.com>
2024-12-19 17:24:10 +0300

    refactor: StorageService instance

--- src/shared/services/index.ts
+++ src/shared/services/index.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-import { ImageService } from './Image'
+import { StorageService } from './Storage'
 
 export const supabaseClient = createClient(
 	import.meta.env.VITE_SUPABASE_URL,
 	import.meta.env.VITE_SUPABASE_ANON_KEY
 )
 
-export const imageService = new ImageService(supabaseClient)
+export const storageService = new StorageService(supabaseClient)
