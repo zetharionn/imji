df282a669724b31c697003db52e9450baeaa5d2e
zetharionn <zetharionn@gmail.com>
2024-12-19 06:21:49 +0300

    refactor: routing as routing app segment

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Routing } from './ui'
+import { Routing } from './routing'
 import './styles'
 
 export const App = () => {
