8ccddf5afdf4d4f5c7d6f40f0ea75263b0f8f51b
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-22 12:41:57 +0300

    feat: publicUrl

--- src/shared/services/Storage/Storage.types.ts
+++ src/shared/services/Storage/Storage.types.ts
@@ -1,8 +1,13 @@
 export interface IStorageService {
 	upload: (bucket: string, path: string, file: File) => UploadResponse
+	publicUrl: (bucket: string, path: string) => PublicUrlResponse
 }
 
 export type UploadResponse = Promise<{
 	data: { id: string; path: string; fullPath: string } | null
 	error: null | Error
+}>
+
+export type PublicUrlResponse = Promise<{
+	data: { publicUrl: string }
 }>
