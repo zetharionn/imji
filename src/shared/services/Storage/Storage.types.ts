a63aa10088168d81a42a8eacc3f685f8c42abe5e
zetharionn <zetharionn@gmail.com>
2024-12-22 12:37:38 +0300

    refactor: StorageService

--- src/shared/services/Storage/Storage.types.ts
+++ src/shared/services/Storage/Storage.types.ts
@@ -1,32 +1,8 @@
 export interface IStorageService {
-	upload: (
-		bucket: string,
-		path: string,
-		fileBody: FileBody,
-		fileOptions?: FileOptions
-	) => StorageResponse
+	upload: (bucket: string, path: string, file: File) => UploadResponse
 }
 
-export type FileBody =
-	| ArrayBuffer
-	| ArrayBufferView
-	| Blob
-	| Buffer
-	| File
-	| FormData
-	| NodeJS.ReadableStream
-	| ReadableStream<Uint8Array>
-	| URLSearchParams
-	| string
-
-export interface FileOptions {
-	cacheControl?: string
-	contentType?: string
-	duplex?: string
-	upsert?: boolean
-}
-
-export type StorageResponse = Promise<{
+export type UploadResponse = Promise<{
 	data: { id: string; path: string; fullPath: string } | null
 	error: null | Error
 }>
