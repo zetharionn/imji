675696afeecc780c106bf65782141cd257fac939
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-22 11:39:28 +0300

    fix: errors

--- src/entities/images/model/store/images.store.ts
+++ src/entities/images/model/store/images.store.ts
@@ -4,6 +4,9 @@ import { storageService } from '@shared/services'
 
 export const useImagesStore = create<IImagesStore>(() => ({
 	upload(file: File) {
-		return storageService.upload('images', file.name, file).then(res => res)
+		return storageService.upload('images', file.name, file).then(res => {
+			if (res.error) throw res.error
+			return res
+		})
 	}
 }))
