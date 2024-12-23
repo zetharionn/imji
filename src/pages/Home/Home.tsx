58bdc188b343ead4c2ebd14a8b85bafc24ecbb50
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-23 18:46:29 +0300

    refactor: title to page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,15 @@ import { Uploader } from '@widgets/Uploader'
 
 export const Home = () => {
 	return (
-		<div className='h-[80vh] flex text-center items-center justify-center'>
-			<Uploader />
+		<div className='h-[80vh] flex flex-col text-center items-center justify-center gap-4'>
+			<div>
+				<h1 className='sm:text-2xl md:text-3xl lg:text-4xl font-bold'>
+					Save and share your images
+				</h1>
+			</div>
+			<div>
+				<Uploader />
+			</div>
 		</div>
 	)
 }
