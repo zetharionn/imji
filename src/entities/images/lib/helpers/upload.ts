389841cc415dde87aebd18e3405cf1e461a3ee29
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-23 19:30:18 +0300

    feat: ImagesSchema

--- src/entities/images/lib/helpers/upload.ts
+++ src/entities/images/lib/helpers/upload.ts
@@ -1,7 +1,10 @@
-import { type UploadResponse, storageService } from '@shared/services'
+import { ImagesSchema } from '@entities/images'
+import { storageService } from '@shared/services'
+import * as v from 'valibot'
 
-export const upload = (file: File): UploadResponse => {
-	return storageService.upload('images', file.name, file).then(res => {
+export const upload = (file: File) => {
+	const result = v.parse(ImagesSchema, file)
+	return storageService.upload('images', result.name, result).then(res => {
 		if (res.error) throw res.error
 		return res
 	})
