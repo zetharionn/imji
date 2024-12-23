a62492b9c009e3710ee4967ef49faadbebe0d6f7
zetharionn <zetharionn@gmail.com>
2024-12-23 18:46:29 +0300

    refactor: title to page

--- src/widgets/Uploader/ui/Uploader.tsx
+++ src/widgets/Uploader/ui/Uploader.tsx
@@ -6,11 +6,6 @@ export const Uploader = () => {
 
 	return (
 		<div className='flex flex-col text-center items-center justify-center gap-4'>
-			<div>
-				<h1 className='sm:text-2xl md:text-3xl lg:text-4xl font-bold'>
-					Save and share your images
-				</h1>
-			</div>
 			<div className='flex flex-col gap-4 w-64'>
 				<FileButton
 					onFile={file => mutation.mutate(file)}
