c21607cd2f454f610a90da2e91b0302a95d85fae
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-23 18:12:35 +0300

    refactor: pages styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,8 @@ import { Uploader } from '@widgets/Uploader'
 
 export const Home = () => {
 	return (
-		<>
+		<div className='h-[80vh] flex text-center items-center justify-center'>
 			<Uploader />
-		</>
+		</div>
 	)
 }
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,9 @@
 export const NotFound = () => {
 	return (
-		<>
-			<div className='h-[80vh] flex flex-col items-center justify-center'>
+		<div className='h-[80vh] flex text-center items-center justify-center'>
+			<div>
 				<h1 className='text-3xl font-bold'>Not Found</h1>
 			</div>
-		</>
+		</div>
 	)
 }
