3fe2e0e7aff842a0e1004b673826fa31a9735009
zetharionn <zetharionn@gmail.com>
2024-12-23 19:30:18 +0300

    feat: ImagesSchema

--- src/entities/images/lib/helpers/publicUrl.ts
+++ src/entities/images/lib/helpers/publicUrl.ts
@@ -1,6 +1,6 @@
-import { type PublicUrlResponse, storageService } from '@shared/services'
+import { storageService } from '@shared/services'
 
-export const publicUrl = (path: string): PublicUrlResponse => {
+export const publicUrl = (path: string) => {
 	return storageService.publicUrl('images', path).then(res => {
 		if (!res.data) throw Error("Can't get public url")
 		return res
