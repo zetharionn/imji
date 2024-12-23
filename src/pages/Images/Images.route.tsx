caa2ff0b4588008339c0d5d0e662601630b893d2
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-23 18:24:03 +0300

    refactor: Images

--- src/pages/Images/Images.route.tsx
+++ src/pages/Images/Images.route.tsx
@@ -1,9 +1,18 @@
 import { rootRoute } from '@pages/Root'
 import { createRoute } from '@tanstack/react-router'
 import { Images } from './Images'
+import { storageService } from '@shared/services'
 
 export const imagesRoute = createRoute({
 	getParentRoute: () => rootRoute,
-	path: '/images/$image',
-	component: Images
+	path: '/images/$imageId',
+	loader: async ({ params: { imageId } }) => {
+		const image = await storageService
+			.publicUrl('images', imageId)
+			.then(res => res.data.publicUrl)
+		return { image }
+	},
+	component: ({ image }) => {
+		return <Images image={image} />
+	}
 })
