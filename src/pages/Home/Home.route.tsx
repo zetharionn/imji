d6d54cc3e5eed004a23005a7ee0f98b1b8914def
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-23 18:53:06 +0300

    chore: constants and routes

--- src/pages/Home/Home.route.tsx
+++ src/pages/Home/Home.route.tsx
@@ -1,9 +1,10 @@
 import { rootRoute } from '@pages/Root'
 import { createRoute } from '@tanstack/react-router'
 import { Home } from './Home'
+import { ROUTES } from '@shared/lib'
 
 export const homeRoute = createRoute({
 	getParentRoute: () => rootRoute,
-	path: '/',
+	path: ROUTES.HOME,
 	component: Home
 })
--- src/pages/Images/Images.route.tsx
+++ src/pages/Images/Images.route.tsx
@@ -2,10 +2,11 @@ import { publicUrl } from '@entities/images'
 import { rootRoute } from '@pages/Root'
 import { createRoute } from '@tanstack/react-router'
 import { Images } from './Images'
+import { ROUTES } from '@shared/lib'
 
 export const imagesRoute = createRoute({
 	getParentRoute: () => rootRoute,
-	path: '/images/$imageId',
+	path: ROUTES.IMAGES,
 	loader: async ({ params: { imageId } }) => {
 		const image = await publicUrl(imageId).then(res => res.data.publicUrl)
 		return { image }
