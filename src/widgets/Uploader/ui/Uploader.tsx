dded19e3b410356b100fedd232b47296b1e5fdca
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-24 17:42:41 +0300

    feat: Alerts

--- src/widgets/Uploader/ui/Uploader.tsx
+++ src/widgets/Uploader/ui/Uploader.tsx
@@ -1,9 +1,29 @@
-import { UploadButton } from '@features/UploadButton'
+import { UploadButton, useUploadButton } from '@features/UploadButton'
+import { Alert, Link } from '@nextui-org/react'
+import { ROUTES } from '@shared/lib'
 
 export const Uploader = () => {
+	const { data, error, status } = useUploadButton()
+
 	return (
 		<div className='flex flex-col text-center items-center justify-center gap-4'>
 			<UploadButton />
+			<Alert
+				isVisible={status === 'success'}
+				color='success'
+				title='Success'
+				description={
+					<Link color='success' href={`${ROUTES.IMAGES}/${data?.data?.path}`}>
+						{data?.data?.path}
+					</Link>
+				}
+			/>
+			<Alert
+				isVisible={status === 'error'}
+				color='danger'
+				title='Error'
+				description={error?.message}
+			/>
 		</div>
 	)
 }
