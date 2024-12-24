5b21ea04f97f50fb65e4022439c074b3dd120b0f
zetharionn <127628065+zetharionn@users.noreply.github.com>
2024-12-24 15:50:03 +0300

    feat: UploadButton

---
+++ src/features/UploadButton/ui/UploadButton.tsx
@@ -0,0 +1,18 @@
+import { FileButton } from '@shared/ui'
+import { useMutation } from '@tanstack/react-query'
+import { upload } from '@entities/images'
+
+export const UploadButton = () => {
+	const mutation = useMutation({
+		mutationFn: upload
+	})
+
+	return (
+		<FileButton
+			onFile={file => mutation.mutate(file)}
+			isLoading={mutation.isPending}
+		>
+			Choose file
+		</FileButton>
+	)
+}
