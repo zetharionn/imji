42e7c980080b272b191c0476bb38da900b93ae6d
zetharionn <zetharionn@gmail.com>
2024-12-24 15:50:03 +0300

    feat: UploadButton

--- src/widgets/Uploader/ui/Uploader.tsx
+++ src/widgets/Uploader/ui/Uploader.tsx
@@ -1,39 +1,9 @@
-import { FileButton, useFileButton } from '@features/FileButton'
-import { Alert, Link } from '@nextui-org/react'
-import { ROUTES } from '@shared/lib'
+import { UploadButton } from '@features/UploadButton'
 
 export const Uploader = () => {
-	const { mutation } = useFileButton()
-
 	return (
 		<div className='flex flex-col text-center items-center justify-center gap-4'>
-			<div className='flex flex-col gap-4 w-64'>
-				<FileButton
-					onFile={file => mutation.mutate(file)}
-					isLoading={mutation.isPending}
-				/>
-				<Alert
-					isVisible={mutation.isSuccess}
-					title='Uploaded successfully'
-					description={
-						<Link
-							color='success'
-							href={`${ROUTES.IMAGES}/${mutation.data?.data?.path}`}
-						>
-							Click here to view the image
-						</Link>
-					}
-					color='success'
-					variant='bordered'
-				/>
-				<Alert
-					isVisible={mutation.isError}
-					title='Error'
-					description={mutation.error?.message}
-					color='danger'
-					variant='bordered'
-				/>
-			</div>
+			<UploadButton />
 		</div>
 	)
 }
