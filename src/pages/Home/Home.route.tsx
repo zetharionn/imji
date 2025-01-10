597cad81f7c353ea0b1be9f1f76d67f104614728
zetharionn <zetharionn@gmail.com>
2025-01-10 04:43:37 +0300

    refactor: pages export default

--- src/pages/Home/Home.route.tsx
+++ src/pages/Home/Home.route.tsx
@@ -1,7 +1,7 @@
 import { rootRoute } from '@pages/Root'
 import { ROUTES } from '@shared/lib'
 import { createRoute } from '@tanstack/react-router'
-import { Home } from './Home'
+import Home from './Home'
 
 export const homeRoute = createRoute({
 	getParentRoute: () => rootRoute,
