d7ede393ef3c45cc46eea7f39482a19d3894392f
zetharionn <zetharionn@gmail.com>
2025-01-11 07:17:03 +0300

    style: compact return

--- src/entities/image/lib/hooks/useUpload.ts
+++ src/entities/image/lib/hooks/useUpload.ts
@@ -1,11 +1,8 @@
 import { useMutation } from '@tanstack/react-query'
 import { upload } from '../helpers'
 
-export const useUpload = () => {
-	const mutation = useMutation({
+export const useUpload = () =>
+	useMutation({
 		mutationKey: ['upload'],
 		mutationFn: upload
 	})
-
-	return mutation
-}
