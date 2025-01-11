4197c580bf38eed7382f2653fc5ed19141bdb0a1
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-01-11 07:17:03 +0300

    style: compact return

--- src/entities/image/lib/hooks/useRetrieve.ts
+++ src/entities/image/lib/hooks/useRetrieve.ts
@@ -1,13 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { retrieve } from '../helpers'
 
-export const useRetrieve = (path: string) => {
-	const query = useQuery({
+export const useRetrieve = (path: string) =>
+	useQuery({
 		queryKey: ['retrieve'],
 		queryFn: () => retrieve(path),
 		gcTime: 0,
 		retry: false
 	})
-
-	return query
-}
