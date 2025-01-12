c98b2a63b7aab3ad409500d0cc5bcc9df2b4c817
zetharionn <zetharionn@gmail.com>
2025-01-12 04:57:36 +0300

    feat: StorageService list

--- src/shared/api/services/storage/storage.types.ts
+++ src/shared/api/services/storage/storage.types.ts
@@ -4,6 +4,11 @@ export interface IStorageService {
 	move: (path: string, newPath: string) => Promise<MoveResponse>
 	remove: (paths: string[]) => Promise<RemoveResponse>
 	download: (path: string) => Promise<DownloadResponse>
+	list: (
+		path?: string,
+		options?: SearchOptions,
+		parameters?: FetchParameters
+	) => Promise<ListResponse>
 }
 
 export interface UploadResponse {
@@ -24,4 +29,46 @@ export interface RemoveResponse {
 export interface DownloadResponse {
 	data: Blob | null
 	error: Error | null
+}
+
+export interface ListResponse {
+	data: FileObject[] | null
+	error: Error | null
+}
+
+export interface Bucket {
+	created_at: string
+	id: string
+	name: string
+	owner: string
+	public: boolean
+	updated_at: string
+	allowed_mime_types?: string[]
+	file_size_limit?: number
+}
+
+export interface FileObject {
+	bucket_id: string
+	buckets: Bucket
+	created_at: string
+	id: string
+	last_accessed_at: string
+	metadata: Record<string, unknown>
+	name: string
+	owner: string
+	updated_at: string
+}
+
+export interface SearchOptions {
+	limit?: number
+	offset?: number
+	search?: string
+	sortBy?: {
+		column?: string
+		order?: string
+	}
+}
+
+export interface FetchParameters {
+	signal?: AbortSignal
 }
