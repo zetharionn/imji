ee6ab7558dfe809f05a735f345982417cad07580
zetharionn <zetharionn@gmail.com>
2025-01-12 06:54:21 +0300

    feat: Counter

---
+++ src/features/Counter/ui/Counter.tsx
@@ -0,0 +1,27 @@
+import { animate, motion, useMotionValue } from 'framer-motion'
+import { useCounter } from '../lib'
+import { useEffect, useState } from 'react'
+
+export const Counter = () => {
+	const { amount } = useCounter()
+	const motionAmount = useMotionValue(0)
+	const [value, setValue] = useState(0)
+
+	useEffect(() => {
+		const animation = animate(motionAmount, amount, {
+			duration: 3,
+			ease: 'easeInOut',
+			onUpdate: value => {
+				setValue(Math.round(value))
+			}
+		})
+
+		return () => animation.stop()
+	}, [motionAmount, amount])
+
+	return (
+		<div>
+			<motion.p className='text-xl font-medium'>{value} Images Hosted</motion.p>
+		</div>
+	)
+}
