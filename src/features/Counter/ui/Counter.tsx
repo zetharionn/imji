81ef2d0e57119071a1cc0742a4d25e827cb994ae
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-01-12 17:10:53 +0300

    refactor: AnimatedValue, memoization

--- src/features/Counter/ui/Counter.tsx
+++ src/features/Counter/ui/Counter.tsx
@@ -1,32 +1,18 @@
-import { animate, motion, useMotionValue } from 'framer-motion'
 import { useCounter } from '../lib'
-import { useEffect, useState } from 'react'
 import { Image } from '@nextui-org/react'
-import { image } from '@shared/ui'
+import { AnimatedValue, image } from '@shared/ui'
+import { memo } from 'react'
 
-export const Counter = () => {
+export const Counter = memo(() => {
 	const { amount } = useCounter()
-	const motionAmount = useMotionValue(0)
-	const [value, setValue] = useState(0)
-
-	useEffect(() => {
-		const animation = animate(motionAmount, amount, {
-			duration: 3,
-			ease: 'easeOut',
-			onUpdate: value => {
-				setValue(Math.round(value))
-			}
-		})
-
-		return () => animation.stop()
-	}, [motionAmount, amount])
 
 	return (
 		<div className='flex flex-row items-center'>
 			<Image src={image} />
-			<motion.p className='text-sm text-gray-300 font-medium'>
-				{value} Images Hosted
-			</motion.p>
+			<p>
+				<AnimatedValue value={amount} duration={3} ease='easeOut' />
+				<span> Images Hosted</span>
+			</p>
 		</div>
 	)
-}
+})
