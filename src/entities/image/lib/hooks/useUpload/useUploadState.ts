93c66d8ada94f1b521bfa86926b1f2870b7c1c53
zetharionn <zetharionn@gmail.com>
2025-01-28 21:38:13 +0300

    refactor: useRetrieveState, useUploadState return destructured object

--- src/entities/image/lib/hooks/useUpload/useUploadState.ts
+++ src/entities/image/lib/hooks/useUpload/useUploadState.ts
@@ -1,13 +1,9 @@
-import type { UploadResponse } from '@shared/api'
 import { useMutationState } from '@tanstack/react-query'
 
 export const useUploadState = () => {
 	const mutation = useMutationState({
 		filters: { mutationKey: ['upload'] }
 	})
-	const data = mutation[mutation.length - 1]?.data as UploadResponse
-	const error = mutation[mutation.length - 1]?.error
-	const status = mutation[mutation.length - 1]?.status
 
-	return { data, error, status }
+	return { ...mutation[mutation.length - 1] }
 }
