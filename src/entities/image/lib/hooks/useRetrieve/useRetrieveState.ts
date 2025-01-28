66d2ca26fec4327f9514d2df57a6ae23193297a7
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-01-28 21:38:13 +0300

    refactor: useRetrieveState, useUploadState return destructured object

--- src/entities/image/lib/hooks/useRetrieve/useRetrieveState.ts
+++ src/entities/image/lib/hooks/useRetrieve/useRetrieveState.ts
@@ -3,9 +3,6 @@ import { useQueryClient } from '@tanstack/react-query'
 export const useRetrieveState = () => {
 	const queryClient = useQueryClient()
 	const query = queryClient.getQueryState(['retrieve'])
-	const data = query?.data
-	const error = query?.error
-	const status = query?.status
 
-	return { data, error, status }
+	return { ...query }
 }
