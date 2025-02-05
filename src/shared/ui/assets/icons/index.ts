44b1095fa9b4d6adf4f24d27206a631d2e23e81a
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-02-05 22:11:44 +0300

    refactor: imji-mark to assets

--- src/shared/ui/assets/icons/index.ts
+++ src/shared/ui/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as bolt } from './bolt.svg'
 export { default as image } from './image.svg'
 export { default as upload } from './upload.svg'
+export { default as imjiMark } from './imji-mark.svg'
