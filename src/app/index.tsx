9d8995f77eb239d45cbb0f0e9378df6f59f2c5be
zetharionn <zetharionn@gmail.com>
2025-02-07 23:48:07 +0300

    feat: Head

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,10 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Routing } from './routing'
 import './styles'
-import { imjiMark } from '@shared/ui'
 
 export const App = () => {
 	return (
 		<>
-			<link rel='icon' type='image/svg+xml' href={imjiMark} />
 			<title>Imji</title>
 			<Analytics />
 			<Routing />
