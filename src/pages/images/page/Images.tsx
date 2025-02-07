75cff2c1307d61601727dc4f93c42871a357b912
zetharionn <zetharionn@gmail.com>
2025-02-08 00:43:29 +0300

    chore: title

--- src/pages/images/page/Images.tsx
+++ src/pages/images/page/Images.tsx
@@ -9,10 +9,13 @@ const Images = () => {
 	const path = imagesRoute.useLoaderData()
 
 	return (
-		<div className='h-[calc(100vh-4rem)] flex flex-col items-center justify-center text-center gap-5'>
-			<Viewer path={path} />
-			<Toolkit />
-		</div>
+		<>
+			<title>{`Imji | ${path}`}</title>
+			<div className='h-[calc(100vh-4rem)] flex flex-col items-center justify-center text-center gap-5'>
+				<Viewer path={path} />
+				<Toolkit />
+			</div>
+		</>
 	)
 }
 
