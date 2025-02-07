9210eb2885cc6a067ac927525b7575acd76af903
zetharionn <zetharionn@gmail.com>
2025-02-07 22:50:25 +0300

    feat: page segment

--- src/pages/root/page/Root.tsx
+++ src/pages/root/page/Root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from '@tanstack/react-router'
 import { Nav } from '@widgets/nav'
-import { Devtools, withProviders } from '../../lib'
+import { Devtools, withProviders } from '../lib'
 
 const Root = () => {
 	return (
