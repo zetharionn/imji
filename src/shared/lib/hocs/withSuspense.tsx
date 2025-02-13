1a3b11384ad53ea70a19fb8b99f926e495847b04
zetharionn <zetharionn@gmail.com>
2025-02-13 18:16:52 +0300

    refactor: LoaderComponent props to Fallback

--- src/shared/lib/hocs/withSuspense.tsx
+++ src/shared/lib/hocs/withSuspense.tsx
@@ -1,12 +1,9 @@
 import { type ComponentType, Suspense } from 'react'
 
 export const withSuspense =
-	<T extends object>(
-		Component: ComponentType<T>,
-		LoaderComponent: ComponentType
-	) =>
+	<T extends object>(Component: ComponentType<T>, Fallback: ComponentType) =>
 	(props: T) => (
-		<Suspense fallback={<LoaderComponent />}>
+		<Suspense fallback={<Fallback />}>
 			<Component {...props} />
 		</Suspense>
 	)
