5de5075d6c71ff9d8daea9c9c3e7afb03c62f10d
zetharionn <zetharionn@gmail.com>
2025-02-15 13:49:49 +0300

    refactor: icons, marks to assets

--- src/shared/ui/assets/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as bolt } from './bolt.svg'
-export { default as image } from './image.svg'
-export { default as upload } from './upload.svg'
-export { default as favicon } from './favicon.svg'
