cc2a63e8493396fcabbdeac6c16b4e268521d137
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-02-15 13:22:13 +0300

    refactor: image entity

--- src/entities/image/lib/retrieve.ts
+++ src/entities/image/lib/retrieve.ts
@@ -1,6 +1,6 @@
 import { storageService } from '@shared/api'
 import * as v from 'valibot'
-import { UrlSchema } from '../../model'
+import { UrlSchema } from '../model'
 
 export const retrieve = async (path: string) => {
 	const { data } = await storageService.retrieve(path)
