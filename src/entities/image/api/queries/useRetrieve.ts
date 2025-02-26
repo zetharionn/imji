659cfea2116d6c72272616bc5aa156d00480ea4a
zetharionn <zetharionn@gmail.com>
2025-02-26 21:13:44 +0300

    chore: suspense mode

--- src/entities/image/api/queries/useRetrieve.ts
+++ src/entities/image/api/queries/useRetrieve.ts
@@ -1,16 +1,15 @@
 import { storageService } from '@shared/api'
-import { useQuery } from '@tanstack/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import * as v from 'valibot'
 import { UrlSchema } from '../../model'
 
 export const useRetrieve = (path: string) => {
-	return useQuery({
+	return useSuspenseQuery({
 		queryKey: ['retrieve'],
 		queryFn: async () => {
 			const { data } = await storageService.retrieve(path)
 			return v.parse(UrlSchema, data?.publicUrl)
 		},
-		gcTime: 0,
 		retry: false
 	})
 }
