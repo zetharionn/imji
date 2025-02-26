a8e8fa420962f5a944bcfc3984475fed8ad7f953
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-02-26 21:13:44 +0300

    chore: suspense mode

--- src/widgets/viewer/ui/Viewer.tsx
+++ src/widgets/viewer/ui/Viewer.tsx
@@ -1,19 +1,14 @@
 import { useRetrieve } from '@entities/image'
 import { ImageCard } from '@features/image-card'
-import { Alert, Spinner } from '@heroui/react'
 import type { FC } from 'react'
 import type { IViewer } from './Viewer.types'
 
 export const Viewer: FC<IViewer> = ({ path }) => {
-	const { data, error, status } = useRetrieve(path)
+	const { data } = useRetrieve(path)
 
 	return (
 		<div>
-			{status === 'success' && <ImageCard url={data} />}
-			{status === 'pending' && <Spinner size='lg' />}
-			{status === 'error' && (
-				<Alert color='danger' title='Error' description={error.message} />
-			)}
+			<ImageCard url={data} />
 		</div>
 	)
 }
