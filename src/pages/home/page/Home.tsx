9ed72ee9121c541f9e108e27f988a99df7d4442d
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-02-27 21:18:58 +0300

    refactor: pages height

--- src/pages/home/page/Home.tsx
+++ src/pages/home/page/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
 	return (
 		<>
 			<title>Imji | Home</title>
-			<div className='h-[calc(100vh-4rem)] flex flex-col items-center justify-center text-center gap-5'>
+			<div className='h-dvh flex flex-col items-center justify-center text-center gap-5'>
 				<div className='flex flex-col flex-wrap text-center gap-1'>
 					<h1 className='text-5xl font-bold'>Save and share your images</h1>
 					<p className='text-sm text-neutral-400'>
