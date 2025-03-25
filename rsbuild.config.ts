bec4cf14ee4ecff3d8463f8f096730889768448a
zetharionn <zetharionn@gmail.com>
2025-03-25 17:58:35 +0300

    style: lint

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,6 @@
-import { pluginSass } from '@rsbuild/plugin-sass'
-import { pluginReact } from '@rsbuild/plugin-react'
 import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { pluginSass } from '@rsbuild/plugin-sass'
 
 export default defineConfig({
 	plugins: [pluginReact(), pluginSass()]
