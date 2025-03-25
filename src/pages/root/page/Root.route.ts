33be9e8640ddd398f8a6fc0a40f362c0549fd02b
zetharionn <zetharionn@gmail.com>
2025-03-25 21:33:14 +0300

    refactor: changed deprecated notFoundRoute to notFoundComponent

--- src/pages/root/page/Root.route.ts
+++ src/pages/root/page/Root.route.ts
@@ -1,6 +1,8 @@
 import { createRootRoute } from '@tanstack/react-router'
 import Root from './Root'
+import NotFound from '@pages/not-found'
 
 export const rootRoute = createRootRoute({
-	component: Root
+	component: Root,
+	notFoundComponent: NotFound
 })
