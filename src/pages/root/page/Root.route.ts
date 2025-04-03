3be05e0c198c7775b2db6a4f94889f400559cb44
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-04-03 23:36:55 +0300

    style: lint

--- src/pages/root/page/Root.route.ts
+++ src/pages/root/page/Root.route.ts
@@ -1,6 +1,6 @@
+import NotFound from '@pages/not-found'
 import { createRootRoute } from '@tanstack/react-router'
 import Root from './Root'
-import NotFound from '@pages/not-found'
 
 export const rootRoute = createRootRoute({
 	component: Root,
