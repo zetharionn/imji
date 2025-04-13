c42d021a7da147743b735384ef4689d1ef5eeee3
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-04-13 10:55:07 +0300

    chore(app layout): added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,27 @@
+import type { Metadata } from 'next'
 import type { FC, PropsWithChildren } from 'react'
 import './styles'
+
+export const metadata: Metadata = {
+	applicationName: 'Imji',
+	authors: [{ name: 'zetharionn' }],
+	description:
+		'An open source service for saving and sharing images with anyone, anytime.',
+	robots: 'all',
+	alternates: {
+		canonical: 'https://imji.vercel.app/'
+	},
+	openGraph: {
+		title: 'Imji',
+		description:
+			'An open source service for saving and sharing images with anyone, anytime.'
+	},
+	twitter: {
+		title: 'Imji',
+		description:
+			'An open source service for saving and sharing images with anyone, anytime.'
+	}
+}
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
 	return (
