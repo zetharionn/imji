6c4519e60c279203212a616b63536dc35c7032c8
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-04-14 19:19:42 +0300

    style(root layout): reorder imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import type { FC, PropsWithChildren } from 'react'
-import './styles'
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import './styles'
 
 export const metadata: Metadata = {
 	applicationName: 'Imji',
