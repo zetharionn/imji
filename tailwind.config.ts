3478a459f5c93f71c5525eaa78c9fa56a01de0b8
zetharionn <zetharionn@gmail.com>
2025-04-14 19:21:54 +0300

    chore(tailwind config): heroui integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
+import { heroui } from '@heroui/react'
 import type { Config } from 'tailwindcss'
 
 export default {
-	content: ['./src/**/*.{ts,tsx}']
+	content: [
+		'./src/**/*.{ts,tsx}',
+		'./node_modules/@heroui/theme/dist/**/*.{js,jsx,ts,tsx}'
+	],
+	darkMode: 'class',
+	plugins: [heroui()]
 } satisfies Config
