73c89f2ecbd84b78cffc9081fe5da45a5e4b57e8
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-04-19 05:57:42 +0300

    feat(api): storage service

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,3 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
 import { QueryClient } from '@tanstack/react-query'
+import { BUCKET } from '../constants'
+import { Storage } from './services'
+
+export const supabaseClient = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 export const queryClient = new QueryClient()
+
+export const storage = new Storage(supabaseClient, BUCKET)
