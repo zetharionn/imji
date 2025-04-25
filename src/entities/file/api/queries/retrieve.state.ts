bdce0b071f38c875348d35a1dd32489010ecf9ef
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-04-25 04:09:28 +0300

    feat(file entity): retrieve query, state hooks

---
+++ src/entities/file/api/queries/retrieve.state.ts
@@ -0,0 +1,8 @@
+import { useQueryClient } from '@tanstack/react-query'
+
+export const useRetrieveState = () => {
+	const queryClient = useQueryClient()
+	const query = queryClient.getQueryState(['retrieve'])
+
+	return { ...query }
+}
