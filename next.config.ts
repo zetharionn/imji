22206b403774c315f4f4b1c8fc38d18dfd903fbc
zetharionn <zetharionn@gmail.com>
2025-04-29 18:00:39 +0300

    chore(next config): allow supabase url

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/**`)]
+	}
+}
 
 export default nextConfig
