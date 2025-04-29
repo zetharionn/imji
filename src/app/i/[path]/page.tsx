1a36ad6df88a83f1694fe9c14458f0de67991196
zetharionn <zetharionn@gmail.com>
2025-04-29 18:15:52 +0300

    feat(views): Images page

---
+++ src/app/i/[path]/page.tsx
@@ -0,0 +1,13 @@
+import Images from '@views/images'
+
+const Page = async ({
+	params
+}: {
+	params: Promise<{ path: string }>
+}) => {
+	const { path } = await params
+
+	return <Images path={path} />
+}
+
+export default Page
