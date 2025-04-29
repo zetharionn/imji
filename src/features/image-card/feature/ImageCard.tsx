6d9fc007c9ea93527b2f4a990bfbe8543c902eb2
zetharionn <zetharionn@gmail.com>
2025-04-29 17:42:23 +0300

    feat(features): image card

---
+++ src/features/image-card/feature/ImageCard.tsx
@@ -0,0 +1,21 @@
+import { Card, CardBody, Image } from '@heroui/react'
+import type { IImageCard } from './ImageCard.types'
+import NextImage from 'next/image'
+import type { FC } from 'react'
+
+export const ImageCard: FC<IImageCard> = ({ url }) => {
+	return (
+		<Card>
+			<CardBody className='overflow-hidden'>
+				<Image
+					isBlurred
+					as={NextImage}
+					src={url}
+					width={1000}
+					height={500}
+					className='object-contain w-full h-full rounded-t-lg'
+				/>
+			</CardBody>
+		</Card>
+	)
+}
