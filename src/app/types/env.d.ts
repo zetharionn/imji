3d20c4909b4843efc0874aed63d07e7a714eab0d
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-04 22:23:00 +0300

    fix(types): declare instead of declare global {}

--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -1,9 +1,7 @@
-declare global {
-	namespace NodeJS {
-		interface ProcessEnv {
-			NODE_ENV: 'development' | 'production' | 'test'
-			NEXT_PUBLIC_SUPABASE_URL: string
-			NEXT_PUBLIC_SUPABASE_ANON_KEY: string
-		}
+declare namespace NodeJS {
+	interface ProcessEnv {
+		NODE_ENV: 'development' | 'production' | 'test'
+		NEXT_PUBLIC_SUPABASE_URL: string
+		NEXT_PUBLIC_SUPABASE_ANON_KEY: string
 	}
 }
