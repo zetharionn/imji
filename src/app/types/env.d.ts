21fee512ceb059558fe1225406b0f9134067d733
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-04 22:11:13 +0300

    fix(types): not declared globally interface

--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -1,7 +1,9 @@
-namespace NodeJS {
-	interface ProcessEnv {
-		NODE_ENV: 'development' | 'production' | 'test'
-		NEXT_PUBLIC_SUPABASE_URL: string
-		NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			NODE_ENV: 'development' | 'production' | 'test'
+			NEXT_PUBLIC_SUPABASE_URL: string
+			NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+		}
 	}
 }
