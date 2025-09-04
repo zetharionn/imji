4f966ddf1e14adea7e8e773e5f12efdb21964dce
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-04 22:11:19 +0300

    pnpm: lint

--- src/views/loader/view/Loader.tsx
+++ src/views/loader/view/Loader.tsx
@@ -4,11 +4,9 @@ import { Spinner } from '@heroui/react'
 
 const Loader = () => {
 	return (
-		<>
-			<div className='h-dvh flex flex-col items-center justify-center text-center gap-5'>
-				<Spinner size='lg' variant='wave' label='Waving...' />
-			</div>
-		</>
+		<div className='h-dvh flex flex-col items-center justify-center text-center gap-5'>
+			<Spinner size='lg' variant='wave' label='Waving...' />
+		</div>
 	)
 }
 
