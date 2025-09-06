f3a04cf69a61894bd75d5babcb6cac1851cc2e6b
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-07 00:55:10 +0300

    feat(providers): added convex provider

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -2,16 +2,19 @@
 
 import { ToastProvider } from '@heroui/react'
 import type { FC, PropsWithChildren } from 'react'
+import { Convex } from './Convex'
 import { HeroUI } from './HeroUI'
 import { Query } from './Query'
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<Query>
-			<HeroUI>
-				<ToastProvider placement='bottom-center' />
-				{children}
-			</HeroUI>
-		</Query>
+		<Convex>
+			<Query>
+				<HeroUI>
+					<ToastProvider placement='bottom-center' />
+					{children}
+				</HeroUI>
+			</Query>
+		</Convex>
 	)
 }
