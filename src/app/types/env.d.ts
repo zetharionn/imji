f4b1c6c36ac0a0ab50eab40b486ce4d99e977bdc
zetharionn <zetharionn@gmail.com>
2025-09-06 23:35:38 +0300

    feat(env): added convex env variables

--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
 		NODE_ENV: 'development' | 'production' | 'test'
 		NEXT_PUBLIC_SUPABASE_URL: string
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+		NEXT_PUBLIC_CONVEX_URL: string
 	}
 }
