8b33a648c21ba5d67379aa17f7b96c404cca7978
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-08 20:45:02 +0300

    feat(convex): upload action, url internalMutation, store internalMutation

---
+++ convex/files.ts
@@ -0,0 +1,35 @@
+import { bufferToFile } from '@convex/lib'
+import { v } from 'convex/values'
+import * as valibot from 'valibot'
+import { internal } from './_generated/api'
+import { action, internalMutation } from './_generated/server'
+import { FileSchema } from './model'
+
+export const upload = action({
+	args: { buffer: v.bytes() },
+	handler: async (ctx, args) => {
+		const result = valibot.safeParse(
+			FileSchema,
+			await bufferToFile(args.buffer)
+		)
+		const url = await ctx.runMutation(internal.files.url)
+
+		result.success &&
+			(await fetch(url, {
+				method: 'POST',
+				body: result.output
+			}).then(async response => {
+				const { storageId } = await response.json()
+				await ctx.runMutation(internal.files.store, { id: storageId })
+			}))
+	}
+})
+
+export const url = internalMutation({
+	handler: async ctx => await ctx.storage.generateUploadUrl()
+})
+
+export const store = internalMutation({
+	args: { id: v.string() },
+	handler: async (ctx, args) => await ctx.db.insert('files', { id: args.id })
+})
