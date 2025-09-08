7b506cefd9fe45452f3e89e33bf486c356de2037
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-09 00:02:15 +0300

    refactor(providers): ToastProvider to independent Toast component

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { ToastProvider } from '@heroui/react'
 import type { FC, PropsWithChildren } from 'react'
 import { Convex } from './Convex'
 import { HeroUI } from './HeroUI'
 import { Query } from './Query'
+import { Toast } from './Toast'
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<Convex>
 			<Query>
 				<HeroUI>
-					<ToastProvider placement='bottom-center' />
+					<Toast />
 					{children}
 				</HeroUI>
 			</Query>
