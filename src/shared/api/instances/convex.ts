96adef61c6219f368edb4e1bc1b30b737d22f61e
zetharionn <zetharionn@gmail.com>
2025-09-10 20:11:18 +0300

    feat(convex): connected to tanstack query

--- src/shared/api/instances/convex.ts
+++ src/shared/api/instances/convex.ts
@@ -1,5 +1,7 @@
+import { ConvexQueryClient } from '@convex-dev/react-query'
 import { ConvexReactClient } from 'convex/react'
 
 export const convexClient = new ConvexReactClient(
 	process.env.NEXT_PUBLIC_CONVEX_URL
 )
+export const convexQueryClient = new ConvexQueryClient(convexClient)
--- src/shared/api/instances/query.ts
+++ src/shared/api/instances/query.ts
@@ -1,3 +1,13 @@
 import { QueryClient } from '@tanstack/react-query'
+import { convexQueryClient } from './convex'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			queryKeyHashFn: convexQueryClient.hashFn(),
+			queryFn: convexQueryClient.queryFn()
+		}
+	}
+})
+
+convexQueryClient.connect(queryClient)
