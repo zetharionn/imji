472cee698592047f17ad07fdb2230b1680c644bb
zetharionn <zetharionn@gmail.com>
2025-09-12 19:39:25 +0300

    chore(storage): added file validation to upload method

--- src/entities/storage/api/methods/upload.ts
+++ src/entities/storage/api/methods/upload.ts
@@ -1,4 +1,10 @@
+import { FileSchema } from '@entities/file'
 import { BUCKET, storage } from '@entities/storage'
+import * as v from 'valibot'
 
 export const upload = async (file: File) =>
-	await storage.createFile(BUCKET, crypto.randomUUID(), file)
+	await storage.createFile(
+		BUCKET,
+		crypto.randomUUID(),
+		v.parse(FileSchema, file)
+	)
