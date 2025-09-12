43c5a7aa2b98e0a7e1c2d2c33ed9080a8b538491
zetharionn <zetharionn@gmail.com>
2025-09-12 19:10:21 +0300

    fix(next config): appwrite images remote pattern as object

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
-			new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL}/**`),
-			new URL(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/**`)
+			{
+				protocol: 'https',
+				hostname: process.env.NEXT_PUBLIC_APPWRITE_HOSTNAME
+			}
 		]
 	}
 }
