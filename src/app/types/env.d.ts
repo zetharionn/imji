c4c51b38e81f9634f3f73a7e4663a890c51e80d2
zetharionn <127628065+zetharionn@users.noreply.github.com>
2025-09-12 19:44:57 +0300

    refactor(env declaration): removed useless env variable declaration

--- src/app/types/env.d.ts
+++ src/app/types/env.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
 	interface ProcessEnv {
 		NODE_ENV: 'development' | 'production' | 'test'
 		NEXT_PUBLIC_APPWRITE_PROJECT_ID: string
-		NEXT_PUBLIC_APPWRITE_PROJECT_NAME: string
 		NEXT_PUBLIC_APPWRITE_HOSTNAME: string
 		NEXT_PUBLIC_APPWRITE_ENDPOINT: string
 		NEXT_PUBLIC_APPWRITE_BUCKET: string
