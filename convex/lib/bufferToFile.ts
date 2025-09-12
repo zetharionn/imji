3da58be63512425fd8b80fed64f23c9e6b7da4b4
zetharionn <zetharionn@gmail.com>
2025-09-12 19:11:28 +0300

    refactor: removed convex docker compose, server

--- convex/lib/bufferToFile.ts
+++
@@ -1,8 +0,0 @@
-import { type FileTypeResult, fileTypeFromBuffer } from 'file-type'
-
-export const bufferToFile = async (buffer: ArrayBuffer) => {
-	const { mime } = (await fileTypeFromBuffer(buffer)) as FileTypeResult
-	return new File([buffer], crypto.randomUUID(), {
-		type: mime
-	})
-}
