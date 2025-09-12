7f4ef897e8adb3c325e3bd74c2e307060b4f819d
zetharionn <zetharionn@gmail.com>
2025-09-12 18:59:36 +0300

    refactor(next config): added appwrite endpoint to images remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL}/**`)]
+		remotePatterns: [
+			new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL}/**`),
+			new URL(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/**`)
+		]
 	}
 }
 
